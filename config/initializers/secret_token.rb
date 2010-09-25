12584335056a66e7dc89f35f848b2c7a8f00d3ef
stevenrojascr <stevenrojascr@gmail.com>
2010-09-24 20:31:25 -0600

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlanillaHelp::Application.config.secret_token = 'f8f8e173c414ff392b549f18c54e9b45cf5a81e233acdd01067fa669f17e81cfd04cad8e6401927e0c91c73c61bc2afc6ab6f5d611f1f47fd26170d78518bc15'
