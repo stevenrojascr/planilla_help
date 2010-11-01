a15224bca3cc93e35c5d0e0602c5db1590d23e6c
stevenrojascr <stevenrojascr@gmail.com>
2010-10-31 20:47:05 -0600

    agregados varios validates

--- app/models/empleado.rb
+++ app/models/empleado.rb
@@ -18,5 +18,25 @@
 
 class Empleado < ActiveRecord::Base
     validates :cedula, :nombre, :quincena, :factor_hora_por_quincena, :fch_ingreso, :activo, :presence => true
-    validates :quincena, :factor_hora_por_quincena,:numericality=>{:greater_than_or_equal_to=>0.01} 
+    validates :quincena, :factor_hora_por_quincena, :numericality => { :greater_than_or_equal_to => 0.01}
+    validates :cedula, :uniqueness => true
+    validates :cedula, :format => {
+                      :with => /^[1-9]{1}[0-9]{8}$/, 
+                      :message => "deben ser 9 dígitos" }
+    validates :fch_ingreso, :format => {
+                      :with => /^[0-9]{4}[-][0-9]{2}[-][0-9]{2}$/,
+                      :message => 'debe ser una fecha en formato aaaa-mm-dd' }
+    validates :tel_casa, :format => {
+                      :with => /^[2]{1}[0-9]{7}$/, 
+                      :message => "deben ser 8 dígitos e iniciar con 2" }
+    validates :tel_movil, :format => {
+                      :with => /^[8]{1}[0-9]{7}$/, 
+                      :message => "deben ser 8 dígitos e iniciar con 8" }
+    validates :quincena_mayor_a_salario_minimo
+
+  protected
+    def quincena_mayor_a_salario_minimo
+      errors.add(:quincena, 'debe ser mayor al salario mínimo de 95.000') if quincena.nil? ||
+                    quincena < 95000
+    end
 end
